9e3707fb1c5d7b10de6655b64e08580b67d8f722
dxzone12 <dxzone12@users.noreply.github.com>
2018-12-01 02:26:54 +1030

    updated image downloader
    Allow image downloader to go through multiple different threads

--- WebScraping/ImageDownloader.py
+++ WebScraping/ImageDownloader.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import sys
+import os
 
 # This program when given the url of a thread on 4chan will download all the images from that thread into the IMAGES folder
 # The URL is passed as a command line parameter
@@ -10,21 +11,28 @@ if len(sys.argv) < 2:
     print('Not enough arguments to run')
     exit()
 
-# Get the page
-page = requests.get(sys.argv[1])
-content = page.content
-soup = BeautifulSoup(content, features = 'html.parser')
+# Repeat for all urls in the list
+folderCount = 1
+for url in sys.argv[1:]:
+    # Make the directories
+    os.makedirs('IMAGES/' + str(folderCount))
 
-# identify all the image links on the page
-imageLinks = [a['href'] for a in soup.find_all('a', class_='fileThumb')]
+    # Get the page
+    page = requests.get(sys.argv[1])
+    content = page.content
+    soup = BeautifulSoup(content, features = 'html.parser')
 
-# Download all the image links
-total = len(imageLinks)
-print (str(total) + ' images found')
-count = 0
-for image in imageLinks:
-    r = requests.get('http:' + str(image))
-    filename = image.split('/')[-1]
-    open('IMAGES/' + filename, 'wb').write(r.content)
-    count = count + 1
-    print('completed ' + str(count) + '/' + str(total))
+    # identify all the image links on the page
+    imageLinks = [a['href'] for a in soup.find_all('a', class_='fileThumb')]
+
+    # Download all the image links
+    total = len(imageLinks)
+    print (str(total) + ' images found')
+    count = 0
+    for image in imageLinks:
+        r = requests.get('http:' + str(image))
+        filename = image.split('/')[-1]
+        open('IMAGES/' + filename, 'wb').write(r.content)
+        count = count + 1
+        print('completed ' + str(count) + '/' + str(total))
+    folderCount = folderCount + 1
